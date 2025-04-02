445d17f8c366e45f21160e554eea9f8d400927d8
likith-18-ai <likithsankar57@gmail.com>
2025-04-02 20:00:28 +0530

    Update clustering_and_fitting.py

--- clustering_and_fitting.py
+++ clustering_and_fitting.py
@@ -65,7 +65,9 @@ def perform_clustering(df, col1, col2):
     X_scaled = scaler.fit_transform(X)
     kmeans = KMeans(n_clusters=3, random_state=42, n_init=10)
     labels = kmeans.fit_predict(X_scaled)
-    return labels, X[col1], X[col2], kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1]
+    (return labels, 
+     X[col1], X[col2], 
+     kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1])
 
 
 def plot_clustered_data(labels, x, y, xkmeans, ykmeans):
